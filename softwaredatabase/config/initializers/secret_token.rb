0f63ef0ae913fdacfd0190c8b650f30f29225142
Lachlan de Crespigny <ldecrespigny@gmail.com>
2014-03-10 15:58:42 -0400

    initial commit

---
+++ softwaredatabase/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Softwaredatabase::Application.config.secret_key_base = 'cd7a7fc7d131593651a09884b7914c3db9e3672e9869ed33cdb074a639ee06d64267f609d08832fb5ff084431af0b662f0c6c3d10104219ba27fa5ebf200b96d'
